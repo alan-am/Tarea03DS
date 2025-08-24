9f1e2ef09abff71fcde3b8b9abb1ffacbd527295
robertobravol <riicte@gmail.com>
2025-08-23 22:53:10 -0500

    Se hace la tecnica de pull up field

--- src/main/java/com/example/ChainResponsability/AgenteSoporte.java
+++ src/main/java/com/example/ChainResponsability/AgenteSoporte.java
@@ -4,50 +4,18 @@ import com.example.ReporteIncidencia;
 import com.example.Usuario;
 
 public class AgenteSoporte extends Usuario implements Operador {
-    private String idAgente;
-    private String nombre;
-    private String email;
-    private String telefono;
     private Operador operador;
 
     public AgenteSoporte(String idAgente, String nombre, String email, String telefono) {
         super(idAgente, nombre, email, telefono);
-        this.idAgente = idAgente;
-        this.nombre = nombre;
-        this.email = email;
-        this.telefono = telefono;
     }
 
     public String getIdAgente() {
-        return idAgente;
+        return getId();
     }
 
     public void setIdAgente(String idAgente) {
-        this.idAgente = idAgente;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
+        setId(idAgente);
     }
 
     public Operador getOperador() {
@@ -57,10 +25,10 @@ public class AgenteSoporte extends Usuario implements Operador {
     @Override
     public String toString() {
         return "AgenteSoporte{" +
-                "idAgente='" + idAgente + '\'' +
-                ", nombre='" + nombre + '\'' +
-                ", email='" + email + '\'' +
-                ", telefono='" + telefono + '\'' +
+                "idAgente='" + getId() + '\'' +
+                ", nombre='" + getNombre() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", telefono='" + getTelefono() + '\'' +
                 ", operador=" + operador +
                 '}';
     }
@@ -79,10 +47,10 @@ public class AgenteSoporte extends Usuario implements Operador {
 
     public boolean gestionarConsulta(boolean puedeGestionar) {
         if (puedeGestionar) {
-            System.out.println("- Agente de soporte " + nombre + " está gestionando la consulta.");
+            System.out.println("- Agente de soporte " + getNombre() + " está gestionando la consulta.");
             return true;
         } else if (operador != null && !puedeGestionar) {
-            System.out.println("- Agente de soporte " + nombre + " no puede gestionar la consulta. Pasando al siguiente operador.");
+            System.out.println("- Agente de soporte " + getNombre() + " no puede gestionar la consulta. Pasando al siguiente operador.");
             return false;
         }
         return false;
